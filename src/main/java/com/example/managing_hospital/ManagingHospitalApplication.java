c3916b6ec5f5a149d487e18fd2d429d28e962210
tae0 <mickey470379@gmail.com>
2024-11-21 19:11:27 +0900

    Spring Boot 개발환경 설정

---
+++ src/main/java/com/example/managing_hospital/ManagingHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.managing_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingHospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/managing_hospital/ManagingHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managing_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
