67b94a09455599b6ea96113aa1453fbfaa2f104f
tae0 <mickey470379@gmail.com>
2024-11-29 20:14:56 +0900

    refactor: purchaseDate 변수명 및 타입 변경

--- src/main/java/com/example/managing_hospital/materials/dto/MaterialsDTO.java
+++ src/main/java/com/example/managing_hospital/materials/dto/MaterialsDTO.java
@@ -5,20 +5,20 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @Getter
 @Setter
 public class MaterialsDTO { //자재 이름, 구매 일자, 재고량
     private String name;
-    private Date purchase_date;
+    private LocalDate purchaseDate;
     private int stock;
 
     public Materials toEntity() {
         Materials materials = new Materials();
         materials.setName(name);
-        materials.setPurchaseDate(purchase_date);
+        materials.setPurchaseDate(purchaseDate);
         materials.setStock(stock);
 
         return materials;
