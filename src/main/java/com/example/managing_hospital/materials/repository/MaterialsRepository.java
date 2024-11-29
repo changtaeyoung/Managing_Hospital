77c07f8a91d76cb19beff17da184a7c1094117c1
tae0 <mickey470379@gmail.com>
2024-11-29 20:15:13 +0900

    refactor: findByName 메소드 반환 타입 변경

--- src/main/java/com/example/managing_hospital/materials/repository/MaterialsRepository.java
+++ src/main/java/com/example/managing_hospital/materials/repository/MaterialsRepository.java
@@ -4,13 +4,14 @@ import com.example.managing_hospital.materials.entity.Materials;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MaterialsRepository extends JpaRepository<Materials, UUID> {
-    Optional<Materials> findByName(String name);
-    Optional<Materials> findByNameAndPurchaseDate(String name, Date purDate);
+    List<Materials> findByName(String name);
+    Optional<Materials> findByNameAndPurchaseDate(String name, LocalDate purDate);
 
 }
